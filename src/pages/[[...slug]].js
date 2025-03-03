57a8a3097ef5ebccbb84be84f2106724f37546f7
nyreehinton <157775382+nyreehinton@users.noreply.github.com>
2025-03-03 00:31:00 -0800

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -5,45 +5,29 @@ import { resolveStaticProps } from '../utils/static-props-resolvers';
 import { resolveStaticPaths } from '../utils/static-paths-resolvers';
 
 function Page(props) {
-  const { page, site } = props;
-  const { modelName } = page.__metadata || {};
-
+    const { page, site } = props;
+    const { modelName } = page.__metadata;
     if (!modelName) {
-    console.error('No modelName in page metadata:', page);
-    throw new Error(`Page has no type, page "${props.path || 'unknown'}"`);
-  }
-
-  const PageLayout = getComponent(modelName);
-  if (!PageLayout) {
-    console.error('No layout found for modelName:', modelName);
-    throw new Error(`No page layout matching the page model: ${modelName}`);
-  }
-
-  return <PageLayout page={page} site={site} />;
+        throw new Error(`page has no type, page '${props.path}'`);
+    }
+    const PageLayout = getComponent(modelName);
+    if (!PageLayout) {
+        throw new Error(`no page layout matching the page model: ${modelName}`);
+    }
+    return <PageLayout page={page} site={site} />;
 }
 
 export function getStaticPaths() {
-  const data = allContent();
-  console.log('getStaticPaths data:', data);
-  const paths = resolveStaticPaths(data.pages || []);
-  console.log('getStaticPaths paths:', paths);
-  return { paths, fallback: false };
+    const data = allContent();
+    const paths = resolveStaticPaths(data);
+    return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const data = allContent();
-  console.log('getStaticProps data:', data);
-  const uriPath = '/' + (params.slug || ['apple-suppliers']).join('/');
-  console.log('getStaticProps uriPath:', uriPath);
-  const props = await resolveStaticProps(uriPath, data);
-  console.log('getStaticProps props:', props);
-
-  if (!props.page || typeof props.page !== 'object') {
-    console.error('Invalid page data for uriPath:', uriPath, 'props:', props);
-    return { notFound: true };
-  }
-
-  return { props };
+    const data = allContent();
+    const urlPath = '/' + (params.slug || []).join('/');
+    const props = await resolveStaticProps(urlPath, data);
+    return { props };
 }
 
 export default Page;
