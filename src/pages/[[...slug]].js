f998d2e0589a0d8fc16a76f4718db423c5d231bb
nyreehinton <157775382+nyreehinton@users.noreply.github.com>
2025-03-02 21:05:09 -0800

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,33 +1,39 @@
 import React from 'react';
-import { allContent } from '../utils/local-content';
-import { getComponent } from '../components/components-registry';
-import { resolveStaticProps } from '../utils/static-props-resolvers';
-import { resolveStaticPaths } from '../utils/static-paths-resolvers';
+import { allContent } from '../../utils/local-content';
+import { getComponent } from '../../components/components-registry';
+import { resolveStaticProps } from '../../utils/static-props-resolvers';
+import { resolveStaticPaths } from '../../utils/static-paths-resolvers';
 
 function Page(props) {
-    const { page, site } = props;
-    const { modelName } = page.__metadata;
-    if (!modelName) {
-        throw new Error(`page has no type, page '${props.path}'`);
-    }
-    const PageLayout = getComponent(modelName);
-    if (!PageLayout) {
-        throw new Error(`no page layout matching the page model: ${modelName}`);
-    }
-    return <PageLayout page={page} site={site} />;
+  const { page, site } = props;
+  const { modelName } = page._metadata || {};
+
+  if (!modelName) {
+    throw new Error(`Page has no type, page "${props.path || 'unknown'}"`);
+  }
+
+  const PageLayout = getComponent(modelName);
+  if (!PageLayout) {
+    throw new Error(`No page layout matching the page model: ${modelName}`);
+  }
+
+  return <PageLayout page={page} site={site} />;
 }
 
 export function getStaticPaths() {
-    const data = allContent();
-    const paths = resolveStaticPaths(data);
-    return { paths, fallback: false };
+  const data = allContent();
+  console.log('getStaticPaths data:', data); // Add logging
+  const paths = resolveStaticPaths(data || []);
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-    const data = allContent();
-    const urlPath = '/' + (params.slug || []).join('/');
-    const props = await resolveStaticProps(urlPath, data);
-    return { props };
+  const data = allContent();
+  console.log('getStaticProps data:', data); // Add logging
+  const uriPath = '/' + (params.slug || ['apple-suppliers']).join('/'); // Default to 'apple-suppliers'
+  const props = await resolveStaticProps(uriPath, data || []);
+  console.log('getStaticProps props:', props); // Add logging
+  return { props };
 }
 
 export default Page;
