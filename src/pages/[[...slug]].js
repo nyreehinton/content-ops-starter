c62b7a8f38d36b36cc0d86bf7348cbe6142fb093
nyreehinton <157775382+nyreehinton@users.noreply.github.com>
2025-03-02 21:08:28 -0800

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { allContent } from '../../utils/local-content';
-import { getComponent } from '../../components/components-registry';
-import { resolveStaticProps } from '../../utils/static-props-resolvers';
-import { resolveStaticPaths } from '../../utils/static-paths-resolvers';
+import { allContent } from '../utils/local-content'; // Adjusted path
+import { getComponent } from '../components/components-registry'; // Adjusted path
+import { resolveStaticProps } from '../utils/static-props-resolvers'; // Adjusted path
+import { resolveStaticPaths } from '../utils/static-paths-resolvers'; // Adjusted path
 
 function Page(props) {
   const { page, site } = props;
@@ -22,17 +22,17 @@ function Page(props) {
 
 export function getStaticPaths() {
   const data = allContent();
-  console.log('getStaticPaths data:', data); // Add logging
+  console.log('getStaticPaths data:', data);
   const paths = resolveStaticPaths(data || []);
   return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
   const data = allContent();
-  console.log('getStaticProps data:', data); // Add logging
-  const uriPath = '/' + (params.slug || ['apple-suppliers']).join('/'); // Default to 'apple-suppliers'
+  console.log('getStaticProps data:', data);
+  const uriPath = '/' + (params.slug || ['apple-suppliers']).join('/');
   const props = await resolveStaticProps(uriPath, data || []);
-  console.log('getStaticProps props:', props); // Add logging
+  console.log('getStaticProps props:', props);
   return { props };
 }
 
